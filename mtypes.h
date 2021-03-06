fb0717ecb0817c002d5940f3450a22c239356e1a
Sutinder Saini <sutinder456@gmail.com>
2021-03-06 10:28:19 -0800

    might aswell jump, JUMP!

--- mtypes.h
+++ mtypes.h
@@ -1,13 +1,29 @@
 #include <stdint.h>
 
-// unsigned
+
 typedef uint8_t         u8;
 typedef uint16_t        u16;
 typedef uint32_t        u32;
 typedef uint64_t        u64;
-
-//signed
 typedef int8_t          i8;
 typedef int16_t         i16;
 typedef int32_t         i32;
 typedef int64_t         i64;
+
+struct uint128_t {
+    u8 given_bits[16];
+};
+
+struct uint256_t {
+    u64 given_bits[4];
+};
+
+struct int128_t {
+    i8 given_bits[16];
+};
+
+struct int256_t {
+    i64 given_bits[4];
+};
+
+
