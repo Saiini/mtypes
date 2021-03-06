3a31e3b1f2a23fd0e7bd054e58db10f6567fa772
Sutinder Saini <sutinder456@gmail.com>
2021-03-06 10:57:06 -0800

    remove the tests

--- mtypes.h
+++ mtypes.h
@@ -9,21 +9,3 @@ typedef int8_t          i8;
 typedef int16_t         i16;
 typedef int32_t         i32;
 typedef int64_t         i64;
-
-struct uint128_t {
-    u8 given_bits[16];
-};
-
-struct uint256_t {
-    u64 given_bits[4];
-};
-
-struct int128_t {
-    i8 given_bits[16];
-};
-
-struct int256_t {
-    i64 given_bits[4];
-};
-
-
